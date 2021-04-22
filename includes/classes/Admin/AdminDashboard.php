20116fc4efadbd0d97f6311983dc4aa5c1abd2f6
Md. Shahnawaz Ahmed <shahnawaz.ahmed@bkash.com>
2021-04-22 10:20:02 +0600

    Fix for ID-2168, ID-2160, ID-2161, Table and DB related issues

--- includes/classes/Admin/AdminDashboard.php
+++ includes/classes/Admin/AdminDashboard.php
@@ -336,6 +336,7 @@ class AdminDashboard
         $this->CreateTransactionTable();
         $this->CreateWebhookTable();
         $this->CreateAgreementMappingTable();
+        $this->CreateTransferHistoryTable();
 
     }
 
@@ -355,10 +356,13 @@ class AdminDashboard
                     `invoice_id` VARCHAR(100) NOT NULL UNIQUE,
                     `payment_id` VARCHAR(50) NULL ,
                     `integration_type` VARCHAR(50) NOT NULL,
+                    `mode` VARCHAR(10) NULL,
+                    `intent` VARCHAR(20) NULL,
                     `amount` decimal(15,2) NOT NULL,
                     `currency` VARCHAR(10) NOT NULL,
                     `refund_id` VARCHAR(50) NULL,
-                    `status` VARCHAR(50) NOT NULL default('INITIATED'),
+                    `refund_amount` decimal(15,2) NULL,
+                    `status` VARCHAR(50) NOT NULL default('CREATED'),
                     `datetime` timestamp NULL,
                     PRIMARY KEY  (ID)
             ) $charset_collate;";
@@ -414,6 +418,35 @@ class AdminDashboard
                     `user_id` bigint NOT NULL,
                     `agreement_token` VARCHAR(300) NOT NULL,
                     `datetime` timestamp NOT NULL,
+                    PRIMARY KEY  (ID)
+            ) $charset_collate;";
+
+            require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+            dbDelta($sql);
+            add_option('bkash_agreement_mapping_table_version', $my_products_db_version);
+        }
+    }
+
+    public function CreateTransferHistoryTable()
+    {
+        global $wpdb;
+        $table_name = $wpdb->prefix . "bkash_transfers";
+        $my_products_db_version = '1.2.0';
+        $charset_collate = $wpdb->get_charset_collate();
+
+        if ($wpdb->get_var("SHOW TABLES LIKE '{$table_name}'") != $table_name) {
+
+            $sql = "CREATE TABLE $table_name (
+                    ID bigint NOT NULL AUTO_INCREMENT,
+                    `receiver` VARCHAR(20) NOT NULL,
+                    `amount` decimal(15,2) NOT NULL,
+                    `currency` VARCHAR(3) NOT NULL,
+                    `trx_id` VARCHAR(50) NOT NULL,
+                    `merchant_invoice_no` VARCHAR(80) NOT NULL,
+                    `transactionStatus` VARCHAR(30) NOT NULL,
+                    `b2cFee` VARCHAR(40) NULL,
+                    `initiationTime` timestamp NULL,
+                    `completedTime` timestamp NULL
                     PRIMARY KEY  (ID)
             ) $charset_collate;";
 
